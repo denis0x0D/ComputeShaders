518fd5f43e16cfd5740924907bc8832a839786a8
Denis Khalikov <dennis.khalikov@gmail.com>
2019-03-18 17:35:24 +0300

    [Vulkan] Able to run shader for assignment

--- Vulkan/VkComputeSample.cc
+++ Vulkan/VkComputeSample.cc
@@ -23,18 +23,19 @@
 //
 // For more information, please refer to <http://unlicense.org/>
 
-#include <vulkan/vulkan.h>
+#include <fstream>
+#include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
-#include <iostream>
-#include <fstream>
 #include <unistd.h>
+#include <vulkan/vulkan.h>
 
 using namespace std;
 
 #define BAIL_ON_BAD_RESULT(result)                                             \
   if (VK_SUCCESS != (result)) {                                                \
     fprintf(stderr, "Failure at %u %s\n", __LINE__, __FILE__);                 \
+    fprintf(stderr, "Exit code %d\n", result);                                 \
     exit(-1);                                                                  \
   }
 
@@ -254,12 +255,12 @@ int main(int argc, const char *const argv[]) {
 
     vkGetPhysicalDeviceMemoryProperties(physicalDevices[i], &properties);
 
-    const int32_t bufferLength = 16384;
+    const int32_t bufferLength = 128;
 
     const uint32_t bufferSize = sizeof(int32_t) * bufferLength;
 
     // we are going to need two buffers from this one memory
-    const VkDeviceSize memorySize = bufferSize * 2;
+    const VkDeviceSize memorySize = 128;
 
     // set memoryTypeIndex to an invalid entry in the properties.memoryTypes
     // array
@@ -284,19 +285,28 @@ int main(int argc, const char *const argv[]) {
     const VkMemoryAllocateInfo memoryAllocateInfo = {
         VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO, 0, memorySize, memoryTypeIndex};
 
-    VkDeviceMemory memory;
+    VkDeviceMemory memory1;
     BAIL_ON_BAD_RESULT(
-        vkAllocateMemory(device, &memoryAllocateInfo, 0, &memory));
+        vkAllocateMemory(device, &memoryAllocateInfo, 0, &memory1));
 
-    int32_t *payload;
+    VkDeviceMemory memory2;
     BAIL_ON_BAD_RESULT(
-        vkMapMemory(device, memory, 0, memorySize, 0, (void **)&payload));
+        vkAllocateMemory(device, &memoryAllocateInfo, 0, &memory2));
 
-    for (uint32_t k = 1; k < memorySize / sizeof(int32_t); k++) {
-      payload[k] = rand();
+    int32_t *payload1, *payload2;
+    BAIL_ON_BAD_RESULT(
+        vkMapMemory(device, memory1, 0, memorySize, 0, (void **)&payload1));
+
+    BAIL_ON_BAD_RESULT(
+        vkMapMemory(device, memory2, 0, memorySize, 0, (void **)&payload2));
+
+    for (uint32_t k = 0; k < memorySize / sizeof(int32_t); k++) {
+      payload1[k] = rand();
+      payload2[k] = rand();
     }
 
-    vkUnmapMemory(device, memory);
+    vkUnmapMemory(device, memory1);
+    vkUnmapMemory(device, memory2);
 
     const VkBufferCreateInfo bufferCreateInfo = {
         VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO,
@@ -308,278 +318,22 @@ int main(int argc, const char *const argv[]) {
         1,
         &queueFamilyIndex};
 
+    //Creates buffer and bind
     VkBuffer in_buffer;
     BAIL_ON_BAD_RESULT(
         vkCreateBuffer(device, &bufferCreateInfo, 0, &in_buffer));
 
-    BAIL_ON_BAD_RESULT(vkBindBufferMemory(device, in_buffer, memory, 0));
+    BAIL_ON_BAD_RESULT(vkBindBufferMemory(device, in_buffer, memory1, 0));
 
     VkBuffer out_buffer;
     BAIL_ON_BAD_RESULT(
         vkCreateBuffer(device, &bufferCreateInfo, 0, &out_buffer));
 
-    BAIL_ON_BAD_RESULT(
-        vkBindBufferMemory(device, out_buffer, memory, bufferSize));
-
-    enum {
-      RESERVED_ID = 0,
-      FUNC_ID,
-      IN_ID,
-      OUT_ID,
-      GLOBAL_INVOCATION_ID,
-      VOID_TYPE_ID,
-      FUNC_TYPE_ID,
-      INT_TYPE_ID,
-      INT_ARRAY_TYPE_ID,
-      STRUCT_ID,
-      POINTER_TYPE_ID,
-      ELEMENT_POINTER_TYPE_ID,
-      INT_VECTOR_TYPE_ID,
-      INT_VECTOR_POINTER_TYPE_ID,
-      INT_POINTER_TYPE_ID,
-      CONSTANT_ZERO_ID,
-      CONSTANT_ARRAY_LENGTH_ID,
-      LABEL_ID,
-      IN_ELEMENT_ID,
-      OUT_ELEMENT_ID,
-      GLOBAL_INVOCATION_X_ID,
-      GLOBAL_INVOCATION_X_PTR_ID,
-      TEMP_LOADED_ID,
-      BOUND
-    };
-
-    enum {
-      INPUT = 1,
-      UNIFORM = 2,
-      BUFFER_BLOCK = 3,
-      ARRAY_STRIDE = 6,
-      BUILTIN = 11,
-      BINDING = 33,
-      OFFSET = 35,
-      DESCRIPTOR_SET = 34,
-      GLOBAL_INVOCATION = 28,
-      OP_TYPE_VOID = 19,
-      OP_TYPE_FUNCTION = 33,
-      OP_TYPE_INT = 21,
-      OP_TYPE_VECTOR = 23,
-      OP_TYPE_ARRAY = 28,
-      OP_TYPE_STRUCT = 30,
-      OP_TYPE_POINTER = 32,
-      OP_VARIABLE = 59,
-      OP_DECORATE = 71,
-      OP_MEMBER_DECORATE = 72,
-      OP_FUNCTION = 54,
-      OP_LABEL = 248,
-      OP_ACCESS_CHAIN = 65,
-      OP_CONSTANT = 43,
-      OP_LOAD = 61,
-      OP_STORE = 62,
-      OP_RETURN = 253,
-      OP_FUNCTION_END = 56,
-      OP_CAPABILITY = 17,
-      OP_MEMORY_MODEL = 14,
-      OP_ENTRY_POINT = 15,
-      OP_EXECUTION_MODE = 16,
-      OP_COMPOSITE_EXTRACT = 81,
-    };
-
-    uint32_t shader[] = {
-        // first is the SPIR-V header
-        0x07230203, // magic header ID
-        0x00010000, // version 1.0.0
-        0,          // generator (optional)
-        BOUND,      // bound
-        0,          // schema
-
-        // OpCapability Shader
-        (2 << 16) | OP_CAPABILITY,
-        1,
-
-        // OpMemoryModel Logical Simple
-        (3 << 16) | OP_MEMORY_MODEL,
-        0,
-        0,
-
-        // OpEntryPoint GLCompute %FUNC_ID "f" %IN_ID %OUT_ID
-        (4 << 16) | OP_ENTRY_POINT,
-        5,
-        FUNC_ID,
-        0x00000066,
-
-        // OpExecutionMode %FUNC_ID LocalSize 1 1 1
-        (6 << 16) | OP_EXECUTION_MODE,
-        FUNC_ID,
-        17,
-        1,
-        1,
-        1,
-
-        // next declare decorations
-
-        (3 << 16) | OP_DECORATE,
-        STRUCT_ID,
-        BUFFER_BLOCK,
-
-        (4 << 16) | OP_DECORATE,
-        GLOBAL_INVOCATION_ID,
-        BUILTIN,
-        GLOBAL_INVOCATION,
-
-        (4 << 16) | OP_DECORATE,
-        IN_ID,
-        DESCRIPTOR_SET,
-        0,
-
-        (4 << 16) | OP_DECORATE,
-        IN_ID,
-        BINDING,
-        0,
-
-        (4 << 16) | OP_DECORATE,
-        OUT_ID,
-        DESCRIPTOR_SET,
-        0,
-
-        (4 << 16) | OP_DECORATE,
-        OUT_ID,
-        BINDING,
-        1,
-
-        (4 << 16) | OP_DECORATE,
-        INT_ARRAY_TYPE_ID,
-        ARRAY_STRIDE,
-        4,
-
-        (5 << 16) | OP_MEMBER_DECORATE,
-        STRUCT_ID,
-        0,
-        OFFSET,
-        0,
-
-        // next declare types
-        (2 << 16) | OP_TYPE_VOID,
-        VOID_TYPE_ID,
-
-        (3 << 16) | OP_TYPE_FUNCTION,
-        FUNC_TYPE_ID,
-        VOID_TYPE_ID,
-
-        (4 << 16) | OP_TYPE_INT,
-        INT_TYPE_ID,
-        32,
-        1,
-
-        (4 << 16) | OP_CONSTANT,
-        INT_TYPE_ID,
-        CONSTANT_ARRAY_LENGTH_ID,
-        bufferLength,
-
-        (4 << 16) | OP_TYPE_ARRAY,
-        INT_ARRAY_TYPE_ID,
-        INT_TYPE_ID,
-        CONSTANT_ARRAY_LENGTH_ID,
-
-        (3 << 16) | OP_TYPE_STRUCT,
-        STRUCT_ID,
-        INT_ARRAY_TYPE_ID,
-
-        (4 << 16) | OP_TYPE_POINTER,
-        POINTER_TYPE_ID,
-        UNIFORM,
-        STRUCT_ID,
-
-        (4 << 16) | OP_TYPE_POINTER,
-        ELEMENT_POINTER_TYPE_ID,
-        UNIFORM,
-        INT_TYPE_ID,
-
-        (4 << 16) | OP_TYPE_VECTOR,
-        INT_VECTOR_TYPE_ID,
-        INT_TYPE_ID,
-        3,
-
-        (4 << 16) | OP_TYPE_POINTER,
-        INT_VECTOR_POINTER_TYPE_ID,
-        INPUT,
-        INT_VECTOR_TYPE_ID,
-
-        (4 << 16) | OP_TYPE_POINTER,
-        INT_POINTER_TYPE_ID,
-        INPUT,
-        INT_TYPE_ID,
-
-        // then declare constants
-        (4 << 16) | OP_CONSTANT,
-        INT_TYPE_ID,
-        CONSTANT_ZERO_ID,
-        0,
-
-        // then declare variables
-        (4 << 16) | OP_VARIABLE,
-        POINTER_TYPE_ID,
-        IN_ID,
-        UNIFORM,
-
-        (4 << 16) | OP_VARIABLE,
-        POINTER_TYPE_ID,
-        OUT_ID,
-        UNIFORM,
-
-        (4 << 16) | OP_VARIABLE,
-        INT_VECTOR_POINTER_TYPE_ID,
-        GLOBAL_INVOCATION_ID,
-        INPUT,
-
-        // then declare function
-        (5 << 16) | OP_FUNCTION,
-        VOID_TYPE_ID,
-        FUNC_ID,
-        0,
-        FUNC_TYPE_ID,
-
-        (2 << 16) | OP_LABEL,
-        LABEL_ID,
-
-        (5 << 16) | OP_ACCESS_CHAIN,
-        INT_POINTER_TYPE_ID,
-        GLOBAL_INVOCATION_X_PTR_ID,
-        GLOBAL_INVOCATION_ID,
-        CONSTANT_ZERO_ID,
-
-        (4 << 16) | OP_LOAD,
-        INT_TYPE_ID,
-        GLOBAL_INVOCATION_X_ID,
-        GLOBAL_INVOCATION_X_PTR_ID,
-
-        (6 << 16) | OP_ACCESS_CHAIN,
-        ELEMENT_POINTER_TYPE_ID,
-        IN_ELEMENT_ID,
-        IN_ID,
-        CONSTANT_ZERO_ID,
-        GLOBAL_INVOCATION_X_ID,
-
-        (4 << 16) | OP_LOAD,
-        INT_TYPE_ID,
-        TEMP_LOADED_ID,
-        IN_ELEMENT_ID,
-
-        (6 << 16) | OP_ACCESS_CHAIN,
-        ELEMENT_POINTER_TYPE_ID,
-        OUT_ELEMENT_ID,
-        OUT_ID,
-        CONSTANT_ZERO_ID,
-        GLOBAL_INVOCATION_X_ID,
-
-        (3 << 16) | OP_STORE,
-        OUT_ELEMENT_ID,
-        TEMP_LOADED_ID,
-
-        (1 << 16) | OP_RETURN,
-
-        (1 << 16) | OP_FUNCTION_END,
-    };
+    BAIL_ON_BAD_RESULT(vkBindBufferMemory(device, out_buffer, memory2, 0));
 
     size_t size = 0;
+    const char *filename_to_read =
+        "/home/khalikov/Vulkan/ComputeShaders/Vulkan/kernel_bin.vulkan";
 
     uint32_t *shader_ptr = ReadFromFile(&size, filename_to_read);
 
@@ -620,17 +374,20 @@ int main(int argc, const char *const argv[]) {
     BAIL_ON_BAD_RESULT(vkCreatePipelineLayout(device, &pipelineLayoutCreateInfo,
                                               0, &pipelineLayout));
 
+    const char *kernel_name = "default_function_kernel0";
+
     VkComputePipelineCreateInfo computePipelineCreateInfo = {
         VK_STRUCTURE_TYPE_COMPUTE_PIPELINE_CREATE_INFO,
         0,
         0,
         {VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO, 0, 0,
-         VK_SHADER_STAGE_COMPUTE_BIT, shader_module, "f", 0},
+         VK_SHADER_STAGE_COMPUTE_BIT, shader_module, kernel_name, 0},
         pipelineLayout,
         0,
         0};
 
     VkPipeline pipeline;
+
     BAIL_ON_BAD_RESULT(vkCreateComputePipelines(
         device, 0, 1, &computePipelineCreateInfo, 0, &pipeline));
 
@@ -712,16 +469,21 @@ int main(int argc, const char *const argv[]) {
 
     BAIL_ON_BAD_RESULT(vkQueueWaitIdle(queue));
 
+    //Check the result.
     BAIL_ON_BAD_RESULT(
-        vkMapMemory(device, memory, 0, memorySize, 0, (void **)&payload));
+        vkMapMemory(device, memory1, 0, memorySize, 0, (void **)&payload1));
+    BAIL_ON_BAD_RESULT(
+        vkMapMemory(device, memory2, 0, memorySize, 0, (void **)&payload2));
 
-    for (uint32_t k = 0, e = bufferSize / sizeof(int32_t); k < e; k++) {
-      BAIL_ON_BAD_RESULT(payload[k + e] == payload[k]
+    for (uint32_t k = 0; k < memorySize / sizeof(uint32_t); k++) {
+      std::cout << "x1 " << payload1[k] << " ";
+      std::cout << "x2 " << payload2[k] << " ";
+      BAIL_ON_BAD_RESULT(payload1[k] == payload2[k]
                              ? VK_SUCCESS
                              : VK_ERROR_OUT_OF_HOST_MEMORY);
+      cout << '\n';
     }
 
-    std::cout << "Buffer size " << bufferSize << std::endl;
     std::cout << "End of the execution " << std::endl;
   }
 }
